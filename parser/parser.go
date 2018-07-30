58f14c3da11ef4f8f791bf3d36286c2ac60749ec
Kentaro Umetsu <umetsu.kentaro@adways.net>
2018-07-30 11:18:43 +0900

    ~p41 Parserのテストが通るようになった

--- parser/parser.go
+++ parser/parser.go
@@ -29,5 +29,65 @@ func (parser *Parser) nextToken() {
 }
 
 func (parser *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !parser.currentTokenIs(token.EOF) {
+		statement := parser.parseStatement()
+		if statement != nil {
+			program.Statements = append(program.Statements, statement)
+		}
+		parser.nextToken()
+	}
+
+	return program
+}
+
+func (parser *Parser) parseStatement() ast.Statement {
+	switch parser.currentToken.Type {
+	case token.LET:
+		return parser.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (parser *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: parser.currentToken}
+
+	if !parser.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	statement.Name = &ast.Identifier{Token: parser.currentToken, Value: parser.currentToken.Literal}
+
+	if !parser.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !parser.currentTokenIs(token.SEMICOLON) {
+		parser.nextToken()
+	}
+
+	return statement
+}
+
+func (parser *Parser) currentTokenIs(t token.TokenType) bool {
+	return parser.currentToken.Type == t
+}
+
+func (parser *Parser) peekTokenIs(t token.TokenType) bool {
+	return parser.peekToken.Type == t
+}
+
+// アサーション関数の一つ
+// peekTokenの型をチェックし、その型が正しい場合に限ってnextTokenを呼んでトークンを進める
+func (parser *Parser) expectPeek(t token.TokenType) bool {
+	if parser.peekTokenIs(t) {
+		parser.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
